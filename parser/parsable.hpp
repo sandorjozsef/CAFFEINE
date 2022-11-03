c95380338cd0727e0bfa25e9fad925e297da2dd8
sandorjozsef <sjozsef2000@gmail.com>
2022-11-03 22:24:40 +0100

    some comments

--- parser/parsable.hpp
+++ parser/parsable.hpp
@@ -8,6 +8,9 @@ namespace parsing{
     class Parsable
     {
         public:
+            // this method parses the parameter vector
+            // it returns the unprocessed (remaining) part of the parameter vector (returns an empty vector if all of the parameter vector was parsed)
+            // if something goes wrong, throws exception
             virtual std::vector<unsigned char> parse(std::vector<unsigned char> bytes) = 0;
     };
 
