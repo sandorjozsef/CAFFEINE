d12a35c4455dfd1f145bae237fb5495a4e6a0068
Dávid Dorián <david.dorian99@gmail.com>
2022-11-04 19:36:53 +0100

    Revert "CAFF credits remove unnecessary static int cast"
    This reverts commit e953dabb017b11c89f73dfd1765b122a36345092.

--- parser/caff_animation.cpp
+++ parser/caff_animation.cpp
@@ -11,25 +11,6 @@ vector<unsigned char> CAFF_ANIMATION::parse(vector<unsigned char> bytes)
 
     cout << "... place of parsing CAFF_ANIMATION ..." << endl;
 
-    if (bytes.size() < 8)
-    {
-        throw invalid_argument("CAFF_ANIMATION is too short!");
-    }
-
-    duration = 0;
-    for (int i = 0; i < 8; ++i)
-    {
-        duration += (bytes[i] << (i * 8));
-    }
-    bytes.erase(bytes.begin(), bytes.begin() + 8);
-
-    if (bytes.size() <= 0)
-    {
-        throw invalid_argument("CAFF_ANIMATION no CIFF provided!");
-    }
-
-    ciff = CIFF();
-    ciff.parse(bytes);
 
     return bytes;
 }
